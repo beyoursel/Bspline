295e89d160acb24d33d4f6276210167b2d298538
@ltao <ltao@topxgun.com>
2024-08-12 14:43:17 +0800

    1

--- BspSlim/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/media/mydisk/Doc/daily_work/work_tg/Bspline/BspSlim/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/media/mydisk/Doc/daily_work/work_tg/Bspline/BspSlim/devel;/opt/ros/noetic'.split(';') if '/media/mydisk/Doc/daily_work/work_tg/Bspline/BspSlim/devel;/opt/ros/noetic' != '' else []
