06b79747036e9a840b9f0741ee0a0ee893ea0a66
beyoursel <1747524097@qq.com>
2024-08-18 22:24:26 +0800

    remove abundant folders and push new folder

--- Bsp/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/mydisk/Doc/daily_work/work_tg/Bspline/Bsp/src
-# Build directory: /media/mydisk/Doc/daily_work/work_tg/Bspline/Bsp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("bspline_surface")
--- Bsp/build/bspline_surface/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/media/mydisk/Doc/daily_work/work_tg/Bspline/Bsp/src/bspline_surface/include".split(';') if "/media/mydisk/Doc/daily_work/work_tg/Bspline/Bsp/src/bspline_surface/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "pcl_msgs;pcl_ros;roscpp;rospy;std_msgs;pcl_conversions".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lptc_demo".split(';') if "-lptc_demo" != "" else []
-PROJECT_NAME = "bspline_surface"
-PROJECT_SPACE_DIR = "/media/mydisk/Doc/daily_work/work_tg/Bspline/Bsp/devel"
-PROJECT_VERSION = "0.0.0"
